63ff950ed3351ccf8ef66ea2cab9be4a9bdd3fb2
Owolewa Onimisi Bamidele <owolewaonimisi@gmail.com>
2024-04-09 15:04:07 +0100

    updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,8 +16,8 @@ const Nav = () =>{
  return(
         <nav className="flex md:justify-around">
           <div className="flex w-full lg:w-max">
-            <h1 className=" lg:hidden p-10" onClick={add}><i class="fa-solid fa-bars"></i></h1>
-            <div className="!mx-auto" style={{margin: '0px auto'}}>
+            <h1 className=" lg:hidden p-5 top-5" onClick={add}><i class="fa-solid fa-bars"></i></h1>
+            <div className="!mx-auto p-0" style={{margin: 'auto'}}>
               <img src="/images/logo.jpeg" style={{width: '100px'}} alt="" />
             </div>
           </div>
