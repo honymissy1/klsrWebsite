eeb4be13d3c354343b35e529b127fd536d0212f8
Owolewa Onimisi Bamidele <owolewaonimisi@gmail.com>
2024-04-10 23:04:39 +0100

    Update

--- src/components/ArticlesComponent.jsx
+++ src/components/ArticlesComponent.jsx
@@ -1,31 +1,53 @@
+import { useEffect } from 'react';
+import { register } from 'swiper/element/bundle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
 const ArticleComponent = () =>{
+
+    useEffect(() =>{
+      register()
+
+
+    }, [])
     return (
-        <div className="bg-neutral p-5 !min-h-[400px]">
-            <h1 className="p-5 font-extrabold text-white text-4xl">Programs</h1>
+        <div className='p-10'>
+       <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={2}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+         <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
+        </SwiperSlide>
+        <SwiperSlide>
+         <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
+        </SwiperSlide>
+        <SwiperSlide>
+         <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
+        </SwiperSlide>
+       
 
-            <div className="carousel rounded-box">
-                <div className="carousel-item mx-2">
-                    <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
-                </div> 
-                <div className="carousel-item mx-2">
-                    <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
-                </div> 
+      </Swiper>
 
-                <div className="carousel-item mx-2">
-                    <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
-                </div> 
+                    {/* <div className='w-[300px] h-auto'>
+                    </div> */}
 
-                <div className="carousel-item mx-2">
-                    <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
-                </div> 
-              
-                <div className="carousel-item">
-                    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
-                </div> 
-                <div className="carousel-item">
-                    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
-                </div>
-                </div>
         </div>
     )
 }
