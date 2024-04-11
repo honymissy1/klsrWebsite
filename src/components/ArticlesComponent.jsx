ec6663f17aa6204cb00ce2c40f70113b051aa100
Owolewa Onimisi Bamidele <owolewaonimisi@gmail.com>
2024-04-11 14:39:08 +0100

    Update

--- src/components/ArticlesComponent.jsx
+++ src/components/ArticlesComponent.jsx
@@ -15,13 +15,15 @@ const ArticleComponent = () =>{
 
     }, [])
     return (
+      <div>
+        <h1 className='pt-10 text-center font-extrabold text-4xl text-[#ad9c3b]'>Programs</h1>
         <div className='p-5 min-h-[50vh] flex items-center'>
        <Swiper
         effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
         slidesPerView={1}
-        loop={true}
+       
         coverflowEffect={{
           rotate: 50,
           stretch: 0,
@@ -29,23 +31,32 @@ const ArticleComponent = () =>{
           modifier: 1,
           slideShadows: true,
         }}
+        breakpoints={{
+          650: {
+            slidesPerView: 2
+          },
+
+          800: {
+            slidesPerView: 3
+          }
+        }}
         navigation={true}
         pagination={false}
         modules={[EffectCoverflow, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide className='!w-[300px]'>
+        <SwiperSlide className='!w-[300px] md:!w-[600px]'>
          <img className='' src="/images/designs/psalm61.png" alt="Burger" />
         </SwiperSlide>
-        <SwiperSlide className='!w-[300px]'>
-         <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
+        <SwiperSlide className='!w-[300px] md:!w-[600px]'>
+         <img className="w-full" src="/images/designs/health.png" alt="Burger" />
         </SwiperSlide>
-        <SwiperSlide className='!w-[300px]'>
+        <SwiperSlide className='!w-[300px] md:!w-[600px]'>
          <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
         </SwiperSlide>
        
-        <SwiperSlide className='!w-[300px]'>
-         <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
+        <SwiperSlide className='!w-[300px] md:!w-[600px]'>
+         <img className="w-full" src="/images/designs/health.png" alt="Burger" />
         </SwiperSlide>
       </Swiper>
 
@@ -53,6 +64,8 @@ const ArticleComponent = () =>{
                     </div> */}
 
         </div>
+
+      </div>
     )
 }
 
