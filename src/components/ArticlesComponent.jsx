acad18a9a7082db55e1e71f8bb0d84be4ee4a443
Owolewa Onimisi Bamidele <owolewaonimisi@gmail.com>
2024-04-11 11:08:19 +0100

    Update

--- src/components/ArticlesComponent.jsx
+++ src/components/ArticlesComponent.jsx
@@ -15,12 +15,13 @@ const ArticleComponent = () =>{
 
     }, [])
     return (
-        <div className='p-10'>
+        <div className='p-5 min-h-[50vh] flex items-center'>
        <Swiper
         effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
-        slidesPerView={2}
+        slidesPerView={1}
+        loop={true}
         coverflowEffect={{
           rotate: 50,
           stretch: 0,
@@ -28,21 +29,24 @@ const ArticleComponent = () =>{
           modifier: 1,
           slideShadows: true,
         }}
-        pagination={true}
+        navigation={true}
+        pagination={false}
         modules={[EffectCoverflow, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
+        <SwiperSlide className='!w-[300px]'>
+         <img className='' src="/images/designs/psalm61.png" alt="Burger" />
+        </SwiperSlide>
+        <SwiperSlide className='!w-[300px]'>
          <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
         </SwiperSlide>
-        <SwiperSlide>
-         <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
-        </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className='!w-[300px]'>
          <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
         </SwiperSlide>
        
-
+        <SwiperSlide className='!w-[300px]'>
+         <img className="w-full" src="/images/designs/psalm61.png" alt="Burger" />
+        </SwiperSlide>
       </Swiper>
 
                     {/* <div className='w-[300px] h-auto'>
